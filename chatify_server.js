5891d3ec6a816412c31f840f83399d1d0fc6dcdd
Surfpup <surfpup00@gmail.com>
2012-08-06 13:37:01 -0400

    Added date

--- chatify_server.js
+++ chatify_server.js
@@ -6,23 +6,37 @@ server.listen(8080);
 
 var nowjs = require("now");
 var everyone = nowjs.initialize(server);
-var names = [];
+
+var names = []; //Active users
+var log = []; //Chat history
 
 everyone.now.distributeMessage = function(message){
-  everyone.now.receiveMessage(this.now.name, message);
+
+	everyone.now.receiveMessage(this.now.name, curDate(), message);
 };
 
 nowjs.on('connect', function(){
-
 	everyone.now.updateUserList(this.now.name);
 	for (var i = 0; i < names.length; i++) {
 		this.now.updateUserList(names[i]);
 	}
 	names.push(this.now.name);
- });
+});
  
- nowjs.on('disconnect', function(){
+nowjs.on('disconnect', function(){
 	var index = names.indexOf(this.now.name);
 	names.splice(index, 1);
 	everyone.now.removeUser(this.now.name); 
- });
+});
+
+function curDate(){
+	var d = new Date();
+	var morn = "AM";
+	var hours = d.getHours();
+	if(hours>12) {
+		morn = "PM";
+		hours-=12;
+	}
+	var dStr = hours + ":" + d.getMinutes() + ":"+d.getSeconds() + " " + morn;
+	return dStr;
+}
